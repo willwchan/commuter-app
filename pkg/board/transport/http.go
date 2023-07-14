e9fcd541342df4b680e90b6ffb5029ad7fcbaef8
Will Chan <github.com/willwchan>
2023-07-13 21:10:01 -0700

    adding http and grpc go files

---
+++ pkg/board/transport/http.go
@@ -0,0 +1,60 @@
+package transport
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"os"
+
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/go-kit/log"
+	"github.com/willwchan/commuter-app/internal/util"
+	"github.com/willwchan/commuter-app/pkg/board/endpoints"
+)
+
+func NewHttpHandler(ep endpoints.Set) http.Handler {
+	m := http.NewServeMux()
+
+	m.Handle("/healthz", httptransport.NewServer(
+		ep.ServiceStatusEndpoint,
+		decodeHttpServiceStatusRequest,
+		encodeResponse,
+	))
+
+	return m
+}
+
+func decodeHttpServiceStatusRequest(_ context.Context, _ *http.Request) (interface{}, error) {
+	var req endpoints.ServiceStatusRequest
+	return req, nil
+}
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	if e, ok := response.(error); ok && e != nil {
+		encodeError(ctx, e, w)
+		return nil
+	}
+	return json.NewEncoder(w).Encode(response)
+}
+
+func encodeError(_ context.Context, err error, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	switch err {
+	case util.ErrUnknown:
+		w.WriteHeader(http.StatusNotFound)
+	case util.ErrInvalidArgument:
+		w.WriteHeader(http.StatusBadRequest)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"error": err.Error(),
+	})
+}
+
+var logger log.Logger
+
+func init() {
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+}
