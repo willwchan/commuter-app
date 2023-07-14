14541c569818cf158c151f38d8ebc61e36319933
Will Chan <github.com/willwchan>
2023-07-13 23:21:09 -0700

    base of grpc.go

--- pkg/board/transport/grpc.go
+++ pkg/board/transport/grpc.go
@@ -1,1 +1,11 @@
 package transport
+
+import (
+	grpctransport "github.com/go-kit/kit/transport/grpc"
+)
+
+type grpcServer struct {
+	get           grpctransport.Handler
+	post          grpctransport.Handler
+	serviceStatus grpctransport.Handler
+}
